54598e89df8ec2d245fb0158af7dd225384c8fc1
Goldan32 <adani32@gmail.com>
2025-03-18 20:47:03 +0100

    Set up as ci submodule

--- docker.sh
+++ docker.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+
+# Figure out current folder and climb up
+CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+PROJECT_ROOT="$(realpath "${SCRIPT_DIR}"/..)"
+
+cd "${SCRIPT_DIR}" || exit 1
 docker build -t "esp32-tag" . && \
 docker run -d -it --name "esp32-container" \
-    --mount type=bind,source=./project,target="/home/esp/project" \
+    --mount type=bind,source="${PROJECT_ROOT}",target="/home/esp/project" \
+    --mount type=bind,source="${SCRIPT_DIR}/container_bashrc",target="/home/esp/.bashrc" \
     --device=/dev/ttyUSB0 \
     --device=/dev/ttyUSB1 \
     --privileged \
     "esp32-tag" && \
 docker exec -it "esp32-container" /bin/bash
+
+cd "$CURRENT_DIR" || exit 1
